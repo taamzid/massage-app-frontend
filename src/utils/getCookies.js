dededbc6c0a67bda80ff7041319f88fa684993ac
taamzid <tamjidalam00@gmail.com>
2025-01-08 18:30:13 +0600

    first commit

--- src/utils/getCookies.js
+++ src/utils/getCookies.js
@@ -12,7 +12,8 @@ export const setCookie = (name, value, days) => {
   const date = new Date();
   date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
   const expires = `expires=${date.toUTCString()}`;
-  document.cookie = `${name}=${value};${expires};path=/`;
+  const secure = location.protocol === "https:" ? "secure" : "";
+  document.cookie = `${name}=${value};${expires};path=/;samesite=strict;${secure}`;
 };
 
 // Function to delete a cookie
